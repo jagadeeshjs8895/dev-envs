2d8d87a53133e65b2fb01709e6383f2034c2feda
Christophe Boucharlat <christophe@containo.us>
2019-04-23 10:06:06 +0200

    update to 7.0

--- 01_elk/refresh_env.sh
+++ 01_elk/refresh_env.sh
@@ -13,12 +13,12 @@ configure_elk_filtering (){
 curl -XPOST -D- 'http://localhost:5601/api/saved_objects/index-pattern' \
     -b /tmp/sg_cookies \
     -H 'Content-Type: application/json' \
-    -H 'kbn-version: 6.3.0' \
+    -H 'kbn-version: 7.0.0' \
     -d '{"attributes":{"title":"logstash-*","timeFieldName":"@timestamp"}}'
 }
 bootstrap(){
 	docker-compose up -d
-	while [[ "$(curl -w ''%{http_code}'' -s -o /dev/null -XPOST  'http://localhost:5601/api/saved_objects/index-pattern' -b /tmp/sg_cookies -H 'Content-Type: application/json' -H 'kbn-version: 6.3.0' -d '{"attributes":{"title":"logstash-*","timeFieldName":"@timestamp"}}')" != "200" ]]; do sleep 5; done
+	while [[ "$(curl -w ''%{http_code}'' -s -o /dev/null -XPOST  'http://localhost:5601/api/saved_objects/index-pattern' -b /tmp/sg_cookies -H 'Content-Type: application/json' -H 'kbn-version: 7.0.0' -d '{"attributes":{"title":"logstash-*","timeFieldName":"@timestamp"}}')" != "200" ]]; do sleep 5; done
 }
 
 bootstrap
